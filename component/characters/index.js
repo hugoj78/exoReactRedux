9f54667bd9f38a1a8745696c77ad20a2157bfd19
hugoj78 <hugo@macbookprohj.home>
2021-03-10 12:08:10 +0100

    meh

--- component/characters/index.js
+++ component/characters/index.js
@@ -1,12 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  Button,
-  Text,
-  Alert,
-  View,
-  SafeAreaView,
-} from 'react-native';
 import axios from 'axios';
 
 import {publicKey, timeStamp, hash} from '../../config/api';
@@ -18,26 +10,6 @@ const Character = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [total, setTotal] = useState(0);
   const limit = 15;
-
-  const decrease = () => {
-    if (offSet.numPage > 1) {
-      setOffSet({
-        ...offSet,
-        number: offSet.number - limit,
-        numPage: offSet.numPage - 1,
-      });
-    }
-  };
-
-  const increase = () => {
-    if (offSet.numPage < total / limit) {
-      setOffSet({
-        ...offSet,
-        number: offSet.number + limit,
-        numPage: offSet.numPage + 1,
-      });
-    }
-  };
 
   useEffect(() => {
     axios({
@@ -61,35 +33,15 @@ const Character = () => {
 
   return (
     <>
-      <ShowAllCharacter pokemon={pokemon} />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.buttonLeft}>
-          <Button title="<" onPress={decrease} />
-        </View>
-        <Text>{offSet.numPage}</Text>
-        <View style={styles.buttonRight}>
-          <Button title=">" onPress={increase} />
-        </View>
-      </SafeAreaView>
+      <ShowAllCharacter
+        pokemon={pokemon}
+        offSet={offSet}
+        setOffSet={setOffSet}
+        limit={limit}
+        total={total}
+      />
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  buttonLeft: {
-    textAlign: 'center',
-    right: 50,
-    top: 27,
-  },
-  buttonRight: {
-    textAlign: 'center',
-    left: 50,
-    bottom: 27,
-  },
-});
 
 export default Character;
--- component/characters/showAll/index.js
+++ component/characters/showAll/index.js
@@ -1,10 +1,18 @@
 import React, {useState, useEffect} from 'react';
-import {FlatList} from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  Button,
+  Text,
+  Alert,
+  View,
+  SafeAreaView,
+} from 'react-native';
 import axios from 'axios';
 
 import ShowAllCharacterDetail from '../showAllDetail';
 
-const ShowAllCharacter = (pokemon) => {
+const ShowAllCharacter = ({pokemon, offSet, setOffSet, limit, total}) => {
   const [pokemonDetail, setPokemonDetail] = useState([]);
 
   useEffect(() => {
@@ -28,6 +36,26 @@ const ShowAllCharacter = (pokemon) => {
     });
   };
 
+  const decrease = () => {
+    if (offSet.numPage > 1) {
+      setOffSet({
+        ...offSet,
+        number: offSet.number - limit,
+        numPage: offSet.numPage - 1,
+      });
+    }
+  };
+
+  const increase = () => {
+    if (offSet.numPage < total / limit) {
+      setOffSet({
+        ...offSet,
+        number: offSet.number + limit,
+        numPage: offSet.numPage + 1,
+      });
+    }
+  };
+
   const renderItem = ({item}) => {
     return (
       <>
@@ -41,10 +69,36 @@ const ShowAllCharacter = (pokemon) => {
       <FlatList
         data={pokemonDetail}
         renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={(item, index) => index.toString()}
       />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.buttonLeft}>
+          <Button title="<" onPress={decrease} />
+        </View>
+        <Text>{offSet.numPage}</Text>
+        <View style={styles.buttonRight}>
+          <Button title=">" onPress={increase} />
+        </View>
+      </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonLeft: {
+    textAlign: 'center',
+    right: 50,
+    top: 27,
+  },
+  buttonRight: {
+    textAlign: 'center',
+    left: 50,
+    bottom: 27,
+  },
+});
 
 export default ShowAllCharacter;
